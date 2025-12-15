b9bd222bb58a2600a49a3c307f3a045e9cf07b58
Dimanochie Chidubem <diman_chid@Dimanochies-MacBook-Pro-8.local>
2025-12-15 11:33:05 -0500

    fix attempt at render

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -33,7 +33,7 @@ export interface User {
   isBanned: boolean;
 }
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL //|| "http://localhost:8000/api";
+const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
 
 interface AuthContextType {
   user: User | null;
