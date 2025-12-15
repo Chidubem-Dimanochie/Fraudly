18da6fda18a263e28daeeb4ec083ca860333300b
Dimanochie Chidubem <diman_chid@Dimanochies-MacBook-Pro-8.local>
2025-12-15 05:09:49 -0500

    updated aws config again

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -8,9 +8,15 @@ Amplify.configure({
       loginWith: {
         oauth: {
           domain: "fraudly.auth.us-east-1.amazoncognito.com",
-          scopes: ["openid", "email"], 
-          redirectSignIn: ["https://fraudly-1.onrender.com/auth/callback"],
-          redirectSignOut: ["https://fraudly-1.onrender.com/login"],
+          scopes: ["openid", "email", "profile"],
+          redirectSignIn: [
+            "http://localhost:3000/auth/callback",
+            "https://fraudly-1.onrender.com/auth/callback"
+          ],
+          redirectSignOut: [
+            "http://localhost:3000/login",
+            "https://fraudly-1.onrender.com/login"
+          ],
           responseType: "code",
         },
       },
